bca0bf07aacee043db5c9b8d85e8e34a5973e91f
emreturkan <emreturkan10@gmail.com>
2024-03-14 23:03:16 +0300

    feat: add tech pages items

--- app/techs/page.js
+++ app/techs/page.js
@@ -1,15 +1,17 @@
+import TechsCards from "@/components/ui/techs-cards";
 import { getTechs } from "@/lib/actions/supabase-tech";
 
 const TechsPage = async () => {
   const techs = await getTechs();
 
   return (
-    <div>
-      <ul>
-        {techs?.map((tech) => (
-          <li key={tech.id}>{tech.name}</li>
-        ))}
-      </ul>
+    <div className="grid gap-4">
+      <TechsCards title="languages" techs={techs} />
+      <TechsCards title="frontend" techs={techs} />
+      <TechsCards title="ui" techs={techs} />
+      <TechsCards title="service" techs={techs} />
+      <TechsCards title="store" techs={techs} />
+      <TechsCards title="utils" techs={techs} />
     </div>
   );
 };
---
+++ components/ui/techs-cards.jsx
@@ -0,0 +1,64 @@
+"use client";
+import React from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { cn, toCapitalize } from "@/lib/utils";
+import { motion } from "framer-motion";
+import { container, items } from "@/lib/animation";
+const TechsCards = ({ title, techs }) => {
+  return (
+    <div className="w-full">
+      <div>
+        <h2 className="text-lg font-semibold  border-b">
+          {toCapitalize(title)}
+        </h2>
+      </div>
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="visible"
+        className="grid grid-cols-4 gap-4 mt-4 "
+      >
+        {techs.map(
+          (tech) =>
+            tech.header === title && (
+              <motion.div variants={items}>
+                <Card
+                  key={tech.id}
+                  className="flex items-center justify-between "
+                >
+                  <CardContent>
+                    <div className="flex items-center gap-4">
+                      <Image
+                        src={tech.img}
+                        alt={tech.name}
+                        width={30}
+                        height={30}
+                      />
+                      <div className="grid gap-1">
+                        <h3 className="font-semibold text-sm">{tech.name}</h3>
+                        <div className="flex items-center gap-1">
+                          <div
+                            className={cn("w-2 h-2  rounded-full", {
+                              "bg-red-500": tech.level === "beginner",
+                              "bg-blue-500": tech.level === "intermediate",
+                              "bg-green-500": tech.level === "expert",
+                            })}
+                          ></div>
+                          <p className="text-xs font-light text-slate-500">
+                            {toCapitalize(tech.level)}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </motion.div>
+            )
+        )}
+      </motion.div>
+    </div>
+  );
+};
+
+export default TechsCards;
