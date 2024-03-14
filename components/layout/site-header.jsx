78d3425c812021dfdaaf65a6101e8dd8db53304b
emreturkan <emreturkan10@gmail.com>
2024-03-14 17:20:58 +0300

    refactor: add dynamic navigation links and active tab state to SiteHeader component

--- components/layout/site-header.jsx
+++ components/layout/site-header.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import Link from "next/link";
 import { Menubar, MenubarMenu, MenubarTrigger } from "../ui/menubar";
 import { ModeToggle } from "../ui/darkMode";
 import { Button } from "../ui/button";
 import { GithubIcon } from "lucide-react";
-
+import { motion } from "framer-motion";
 const SiteHeader = () => {
+  const navLinks = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 2, name: "Photos", path: "/photos" },
+    { id: 3, name: "Bookmarks", path: "/techs" },
+    { id: 4, name: "Techs", path: "/techs" },
+  ];
+
+  let [activeTab, setActiveTab] = useState(navLinks[0].id);
   return (
     <header>
       <div className="flex justify-between">
@@ -16,18 +25,36 @@ const SiteHeader = () => {
         </Avatar>
         <Menubar>
           <MenubarMenu>
-            <Link href={"/"}>
-              <MenubarTrigger>Home</MenubarTrigger>
-            </Link>
-            <Link href={"/photos"}>
-              <MenubarTrigger>Photos</MenubarTrigger>
-            </Link>
-            <Link href={"/bookmarks"}>
-              <MenubarTrigger>Bookmarks</MenubarTrigger>
-            </Link>
-            <Link href={"/techs"}>
-              <MenubarTrigger>Techs</MenubarTrigger>
-            </Link>
+            {navLinks.map((link) => (
+              <Link
+                href={link.path}
+                key={link.id}
+                onClick={() => setActiveTab(link.id)}
+                className={`${
+                  activeTab === link.id ? "" : ""
+                } relative rounded-full   text-sm font-medium outline-sky-400 transition focus-visible:outline-2`}
+                style={{
+                  WebkitTapHighlightColor: "transparent",
+                }}
+              >
+                {activeTab === link.id && (
+                  <motion.span
+                    layoutId="bubble"
+                    className="absolute inset-0 z-10 bg-amber-50 mix-blend-difference"
+                    style={{ borderRadius: 9999 }}
+                    animate={{
+                      scale: 1.2,
+                    }}
+                    transition={{
+                      type: "spring",
+                      bounce: 0.2,
+                      duration: 0.6,
+                    }}
+                  />
+                )}
+                <MenubarTrigger>{link.name}</MenubarTrigger>
+              </Link>
+            ))}
           </MenubarMenu>
         </Menubar>
         <div className="flex items-center gap-2">
