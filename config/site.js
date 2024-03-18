4f09aa34331f486039e59991c828972acc1c5241
emreturkan <emreturkan10@gmail.com>
2024-03-18 10:26:42 +0300

    feat: add site configuration object and use it for metadata in RootLayout

---
+++ config/site.js
@@ -0,0 +1,21 @@
+export const siteConfig = {
+  name: "Emre",
+  lastName: "Turkan",
+  description: "Personal website of Emre Turkan, built with Next.js.",
+  url: "https://emreturkan.com",
+  links: {
+    github: "https://github.com/emreturkan",
+    linkedin: "https://www.linkedin.com/in/emreturkan/",
+    twitter: "https://twitter.com/_emreturkan",
+  },
+  keywords: [
+    "nextjs",
+    "react",
+    "react server components",
+    "emre",
+    "emreturkan",
+    "portfolio",
+    "website",
+  ],
+  creator: "emreturkan",
+};
