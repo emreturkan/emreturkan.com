d337fba7ee7e974bce720b0df8c75c3fecd837c7
Emre Turkan <emreturkan10@gmail.com>
2024-03-19 21:41:36 +0300

    feat: Add date-fns library and implement ConvertDate function
    Added the date-fns library to handle date formatting and implemented a ConvertDate function to display relative time since bookmark creation. This enhances the user experience by providing more informative timestamp display.

--- app/bookmarks/page.jsx
+++ app/bookmarks/page.jsx
@@ -1,29 +1,35 @@
-import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { Card, CardDescription } from "@/components/ui/card";
 import { getBookmark } from "@/lib/actions/get-bookmark";
-import Image from "next/image";
+import ConvertDate from "@/lib/date";
+import { LucideYoutube } from "lucide-react";
 import React from "react";
 
 const Bookmarks = async () => {
   const accessToken = await getAccessToken();
   const getBookmarks = await getBookmark(accessToken?.access_token);
 
+  console.log(getBookmarks.items[0]);
+
   return (
-    <div className="w-full h-screen grid grid-cols-2 gap-4 ">
+    <div className="w-full h-screen grid grid-cols-1 gap-4 ">
       {getBookmarks?.items?.map((bookmark) => (
-        <Card key={bookmark._id} className="text-white">
+        <Card key={bookmark._id} className="text-white border-none">
           {bookmark && (
             <div className="grid gap-4">
-              <CardTitle className="text-secondary-foreground text-lg p-2">
-                {bookmark.title}
-              </CardTitle>
               <CardDescription>
-                <Image
-                  src={bookmark.cover}
-                  alt={bookmark.excerpt}
-                  width={400}
-                  height={300}
-                />
-                <span className="text-sm mt-2 p-4"> {bookmark.excerpt}</span>
+                <div className="w-9/12 p-4 grid gap-2">
+                  <h3 className="text-secondary-foreground text-base font-semibold">
+                    {" "}
+                    {bookmark.title}
+                  </h3>
+                  <div className="text-sm">{bookmark.excerpt}</div>
+                  <div className="flex text-secondary-foreground items-center gap-2">
+                    <LucideYoutube />
+                    <span>{bookmark.domain} ·</span>
+                    <span>{ConvertDate(bookmark.created)}</span>
+                  </div>
+                </div>
+                <hr className="mt-2" />
               </CardDescription>
             </div>
           )}
