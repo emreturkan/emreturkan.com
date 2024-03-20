e9a5e9521db20eea4fdb01a69bce490aa394ae81
emreturkan <emreturkan10@gmail.com>
2024-03-20 15:26:35 +0300

    feat: add support for fetching and displaying Steam achievements and details
    The changes introduce new functions to fetch Steam achievements and details for a game. These achievements are then displayed in a carousel component on the user interface. This enhancement allows users to view and track their achievements in a visually appealing way.

--- components/page/home/activity/gameActivity.jsx
+++ components/page/home/activity/gameActivity.jsx
@@ -1,17 +1,48 @@
 import {
   getGameActivity,
   getGameDetails,
+  getSteamAchievement,
+  getSteamAchievementDetails,
   getSteamStats,
 } from "@/lib/actions/get-steam";
 import Image from "next/image";
 import { Clock, Trophy } from "lucide-react";
-import { minToHour } from "@/lib/utils";
+import { cn, minToHour } from "@/lib/utils";
 import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 const GameActivity = async () => {
   const lastActivity = await getGameActivity();
   const gameDetail = await getGameDetails(lastActivity.response.games[0].appid);
   const capsule_image = gameDetail?.header_image;
   const gameInfo = await getSteamStats(lastActivity.response.games[0].appid);
+
+  const actived = await getSteamAchievement(
+    lastActivity.response.games[1].appid
+  );
+
+  const gameAchievements = await getSteamAchievementDetails(
+    lastActivity.response.games[1].appid
+  );
+
+  const activeAchievements = actived.playerstats.achievements.filter(
+    (i) => i.achieved === 1
+  );
+
+  const allAchievements = gameAchievements.game.availableGameStats.achievements;
+
+  const filteredAchievements = allAchievements.filter((achievement) =>
+    activeAchievements.some((active) => active.apiname === achievement.name)
+  );
 
   return (
     <Card>
@@ -31,20 +62,62 @@ const GameActivity = async () => {
               {gameDetail?.short_description}
             </p>
           </div>
-          <div className="flex items-center gap-4">
-            <div className="flex items-center gap-1">
-              <Clock className="w-5 h-5 text-green-400" />
-              <p className="text-xs">
-                {minToHour(lastActivity?.response?.games[0]?.playtime_forever)}{" "}
-                hours played
-              </p>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <div className="flex items-center gap-1">
+                <Clock className="w-5 h-5 text-green-400" />
+                <p className="text-xs">
+                  {minToHour(
+                    lastActivity?.response?.games[0]?.playtime_forever
+                  )}{" "}
+                  hours played
+                </p>
+              </div>
+              <div className="flex items-center gap-1">
+                <Trophy className="w-5 h-5 text-orange-400" />
+                <p className="text-xs">
+                  {gameInfo?.playerstats.achievements[0].achieved} achievements
+                </p>
+              </div>
             </div>
-            <div className="flex items-center gap-1">
-              <Trophy className="w-5 h-5 text-orange-400" />
-              <p className="text-xs">
-                {gameInfo?.playerstats.achievements[0].achieved} achievements
-              </p>
-            </div>
+            <Carousel
+              className={cn("max-w-sm", {
+                "w-12": filteredAchievements.length === 1,
+                "w-32": filteredAchievements.length > 2,
+              })}
+            >
+              <CarouselContent className="-ml-1 ">
+                {filteredAchievements.map((achievement, index) => (
+                  <CarouselItem
+                    key={achievement.name}
+                    className={cn("pl-1 md:basis-1/2 ", {
+                      "basis-full lg:basis-full":
+                        filteredAchievements.length === 1,
+                      "basis-1/3 lg:basis-1/3": filteredAchievements.length > 2,
+                    })}
+                  >
+                    <div className="p-1">
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <Image
+                              key={achievement.name}
+                              src={achievement.icon}
+                              alt={achievement.name}
+                              width={20}
+                              height={20}
+                            />
+                          </TooltipTrigger>
+                          <TooltipContent className="absolute">
+                            {achievement.displayName}
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
           </div>
         </div>
       </CardContent>
