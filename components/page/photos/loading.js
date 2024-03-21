38d910fbbcdd069f9534b3d94840dc746a7212a5
emreturkan <emreturkan10@gmail.com>
2024-03-21 12:01:09 +0300

    feat: add sharp library to manage image processing tasks
    The sharp library has been added to handle image processing tasks efficiently. This library will be used for image manipulation and optimization within the application.

--- components/page/photos/loading.js
+++ components/page/photos/loading.js
@@ -5,7 +5,10 @@ const Loading = () => {
   return (
     <div className="columns-1  gap-3 mt-4 sm:columns-2 lg:columns-2 rounded">
       {Array.from({ length: 20 }).map((_, i) => (
-        <Skeleton className="rounded mb-4 w-[500px] h-[600px] " key={i} />
+        <Skeleton
+          className="rounded mb-4 w-[500px] h-[500px] max-w-sm"
+          key={i}
+        />
       ))}
     </div>
   );
--- components/page/photos/photos-image.jsx
+++ components/page/photos/photos-image.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import sharp from "sharp";
 import { getUnsplashPhotos } from "@/lib/actions/get-unsplash";
-
 const PhotosImage = async () => {
   const photos = await getUnsplashPhotos();
+  const blurredDataUrls = await Promise.all(
+    photos.map(async (photo) => {
+      const response = await fetch(photo.urls.regular);
+      const blob = await response.blob();
+      const buffer = await blob.arrayBuffer();
+      const { data: base64 } = await sharp(buffer)
+        .resize(100, 100)
+        .jpeg({ quality: 10 })
+        .toBuffer({ resolveWithObject: true });
+      return `data:image/jpeg;base64,${base64.toString("base64")}`;
+    })
+  );
+
   return (
     <div className="columns-1 gap-3 mt-4 sm:columns-2 lg:columns-2 rounded">
-      {photos.map((photo) => (
+      {photos.map((photo, index) => (
         <Link
           href={photo.links.html}
           key={photo.id}
@@ -20,7 +33,7 @@ const PhotosImage = async () => {
             width={500}
             height={500}
             placeholder="blur"
-            blurDataURL={photo.urls.regular}
+            blurDataURL={blurredDataUrls[index]}
             loading="lazy"
             className="rounded mb-4 max-w-sm"
           />
