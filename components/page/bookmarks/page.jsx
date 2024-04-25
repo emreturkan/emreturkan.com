fa48b9013a6f26ef17c32a753b66ad281bcea8ed
emreturkan <emreturkan10@gmail.com>
2024-04-25 17:59:34 +0300

    fix

--- components/page/bookmarks/page.jsx
+++ components/page/bookmarks/page.jsx
@@ -7,8 +7,9 @@ import { Dot } from "lucide-react";
 const BookmarksPage = async () => {
   const accessToken = await getAccessToken();
   const getBookmarks = await getBookmark(accessToken?.access_token);
+
   return (
-    <div className="grid grid-cols-1 gap-4  ">
+    <div className="grid grid-cols-1 gap-4   ">
       {getBookmarks?.items?.map((bookmark) => (
         <Card key={bookmark._id} className="shadow-md border p-4 py-2 rounded ">
           {bookmark && (
@@ -46,13 +47,13 @@ export default BookmarksPage;
 const getAccessToken = async () => {
   try {
     const response = await fetch(
-      "https://emreturkan.com/api",
+      "http://emreturkan.com/api",
       {
         method: "POST",
       },
       {
         next: {
-          revalidate: 432000,
+          revalidate: 3600,
         },
       }
     );
