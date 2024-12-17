b63f13fbef8a86456d5b6cd35688e21c6d3fe1b5
emreturkan <emreturkan10@gmail.com>
2024-12-17 14:45:22 +0300

    feat: add snow-fall

---
+++ components/ui/snow-fall.jsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import Snowfall from "react-snowfall";
+
+const SnowfallItem = () => {
+  return <Snowfall color="#fff" snowflakeCount={100} />;
+};
+
+export default SnowfallItem;
