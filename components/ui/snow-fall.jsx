0f7a44d09676d0b3b03c4e0ebc74dcaf933018d8
emreturkan <emreturkan10@gmail.com>
2024-12-23 14:54:40 +0300

    feat: fix snow-fall height

--- components/ui/snow-fall.jsx
+++ components/ui/snow-fall.jsx
@@ -3,7 +3,20 @@ import React from "react";
 import Snowfall from "react-snowfall";
 
 const SnowfallItem = () => {
-  return <Snowfall color="#fff" snowflakeCount={100} />;
+  return (
+    <Snowfall
+      color="#fff"
+      snowflakeCount={100}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100vh", 
+        zIndex: -1, 
+      }}
+    />
+  );
 };
 
 export default SnowfallItem;
